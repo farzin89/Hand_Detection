49e2cba2ccded79f16ffb14a2e0b509aee62d9c6
farzin89 <farzin_ahmadi90@yahoo.com>
2022-07-09 20:38:47 +0200

    Saving pictures and create the model with Teachable machine whit google website and test it

--- Hand_sign_detection_1/model/test.py
+++ Hand_sign_detection_1/model/test.py
@@ -1,18 +1,21 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
+import tensorflow
 import time
 offset = 20
 imageSize = 300
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+classifier = Classifier("keras_model.h5","labels.txt")
 
 #this part is for saving our images in our folders with presing the S
 
 folder = "Data/c"
 counter = 0
-
+labels = ["A","B","C"]
 while True :
     success,img = cap.read()
     hands,img =detector.findHands(img)
@@ -37,6 +40,10 @@ while True :
             # put bbox in center of white image
             wGap = math.ceil((imageSize - wCal)/2)
             imgWhite[:, wGap:wCal+wGap] = imgResize
+            prediction,index = classifier.getPrediction(img)
+            print(prediction,index)
+
+
         else:
             k = imageSize / w
             hCal = math.ceil(k * h)
@@ -50,8 +57,4 @@ while True :
         cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image",img)
-    key= cv2.waitKey(1)
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
-        print(counter)
+    cv2.waitKey(1)
