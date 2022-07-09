54f7bbb7fbca29723942cadabb9bf5fb9b1d02a4
farzin89 <farzin_ahmadi90@yahoo.com>
2022-07-09 18:58:35 +0200

    putting separate bbox on another white picture

--- Hand_sign_detection_1/Hand_sign_detection1.py
+++ Hand_sign_detection_1/Hand_sign_detection1.py
@@ -1,18 +1,29 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
 
 offset = 20
-
+imageSize = 300
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+
 while True :
     success,img = cap.read()
     hands,img =detector.findHands(img)
     if hands:
         hand = hands[0]
         x,y,w,h = hand["bbox"]
-        imgCrop = img[y-offset:y+h+offset,x-offset:x+w+offset]
+
+        imgWhite = np.ones((imageSize,imageSize,3),np.uint8) * 255 # create picture
+
+        imgCrop = img[y-offset:y +h + offset,x-offset:x+w + offset]
+
+        imgCropshape = imgCrop.shape
+
+        imgWhite[0:imgCropshape[0],0:imgCropshape[1]] = imgCrop
+
         cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
